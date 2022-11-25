86b58a60675375e0b50865774d545d96a3453771
levin <levin@theplant.jp>
2022-11-25 11:22:34 +0800

    fix:modity

--- pkg/info.go
+++ pkg/info.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func PrintVersion() string {
 	//version := "pseudo version"
-	version := "version is v1.1.0"
+	version := "version is v2.0.0"
 	fmt.Println(version)
 	return version
 }
